51abd820f849b92fda33ac07059ef7af0cb10224
Aaron Albertina <aaronalbertina@aarons-air.lan>
2022-12-10 20:20:13 -0600

    Day8 Part2

--- Day8/main.py
+++ Day8/main.py
@@ -19,15 +19,19 @@ def main():
         col_count += 1
     
     total_count = 0 
-    
+    tree_num = []
+
     for row_num,values in rows.items():
 
+        tree_n = 1
         row_index = 0
         col_index = 0
         col_num = 0
 
         for value in values:
 
+            value = int(value)
+            
             if row_num == 1 or row_num == len(rows):
                 total_count += 1
             
@@ -36,27 +40,74 @@ def main():
                     if not row_num == 1 or not row_num == len(rows):
                         total_count += 1
             
-                    value = int(value)
                 else:
                     right = values[row_index:][1:]
                     left = values[:row_index]
                     bottom = cols[col_index + 1][row_num:]
                     top = cols[col_index + 1][:row_num - 1]
                     
-                    right = [i for i in right if i >= value]
-                    left = [i for i in left if i >= value]
-                    top = [i for i in top if i >= value]
-                    bottom = [i for i in bottom if i >= value]
+                    right_1 = [i for i in right if int(i) >= value]
+                    left_1 = [i for i in left if int(i) >= value]
+                    top_1 = [i for i in top if int(i) >= value]
+                    bottom_1 = [i for i in bottom if int(i) >= value]
                     
-                    if not right or not left or not top or not bottom:
+                    if not right_1 or not left_1 or not top_1 or not bottom_1:
                         total_count += 1
+                    
+
+                    right = values[row_index:][1:]
+                    left = values[:row_index]
+                    bottom = cols[col_index + 1][row_num:]
+                    top = cols[col_index + 1][:row_num - 1]
+                    
+                    lcount = 0 
+                    rcount = 0
+                    tcount = 0
+                    bcount = 0
+
+                    for x in right:
+                        x = int(x)
+                        if value > x:
+                            rcount += 1
+                        else:
+                            rcount +=1
+                            break
+                    
+                    for x in left[::-1]:
+                        x = int(x)
+                        if value > x:
+                            lcount += 1
+                        else:
+                            lcount +=1
+                            break
+
+                    for x in top[::-1]:
+                        x = int(x)
+                        if value > x:
+                            tcount += 1
+                        else:
+                            tcount +=1
+                            break
+
+                    for x in bottom:
+                        x = int(x)
+                        if value > x:
+                            bcount += 1
+                        else:
+                            bcount +=1
+                            break
+
+                    tree_num.append(lcount * rcount * tcount * bcount)
+
 
             row_index += 1
             col_index += 1
 
     """ PART 1 """            
-    print(total_count)
+    print(f'Part 1 Total: {total_count}')
 
+    """ PART 2 """
+    print(f'Part 2 Total: {max(tree_num)}')
 
 
 
